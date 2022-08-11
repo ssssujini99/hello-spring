c687503df6f593304d8096cf510c62add1043cb1
ssssujini99 <sssssjin99@naver.com>
2022-08-11 22:13:27 +0900

    :sparkles: View 환경설정

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";  // resources/templates/hello.html 을 찾아서 해당 템플릿을 렌더링함
+    }
+}
